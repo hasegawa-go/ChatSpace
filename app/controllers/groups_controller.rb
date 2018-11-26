d3a54d2c0504c49611cb27245cb949ffa488566f
hasegawa-go <gou1982@gmail.com>
2018-11-26 13:18:50 +0900

    Add-async
    What
    チャットスペース非同期通信化
    Why
    実装に必要な為

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -22,17 +22,16 @@ def edit
 end
 
 def update
-  if @group.update(group_params)
+
+ private
+ def group_params
+    params.require(:group).permit(:name, { :user_ids => [] })if @group.update(group_params)
       redirect_to group_messages_path(@group), notice: 'グループを編集しました'
     else
       render :edit
     end
 end
 
- private
- def group_params
-    params.require(:group).permit(:name, { :user_ids => [] })
- end
 
  def set_group
     @group = Group.find(params[:id])
