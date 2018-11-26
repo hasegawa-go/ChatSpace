179dd0a033aef8544a0cf99ec31a4ed79330d7e0
hasegawa-go <gou1982@gmail.com>
2018-11-26 16:13:45 +0900

    ご指摘箇所の修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,44 +1,61 @@
 $(document).on('turbolinks:load', function(){
-     function buildHTML(message){
-      if ( message.image ) {
-        var html =
-         `<div class="message" data-message-id=${message.id}>
-            <div class="upper-message">
-              <div class="upper-message__user-name">
-                ${message.user_name}
-              </div>
-              <div class="upper-message__date">
-                ${message.date}
-              </div>
-            </div>
-            <div class="lower-message">
-              <p class="lower-message__content">
-                ${message.content}
-              </p>
-            </div>
-            <asset_path src=${message.image} >
-          </div>`
-        return html;
-      } else {
-        var html =
-         `<div class="message" data-message-id=${message.id}>
-            <div class="upper-message">
-              <div class="upper-message__user-name">
-                ${message.user_name}
-              </div>
-              <div class="upper-message__date">
-                ${message.date}
-              </div>
-            </div>
-            <div class="lower-message">
-              <p class="lower-message__content">
-                ${message.content}
-              </p>
-            </div>
-          </div>`
-        return html;
-      };
-    }
+//      function buildHTML(message){
+  // $(function(){
+   function buildHTML(message){
+     // var message_content = message.content ? `<p class="lower-message__content">${message.content)</p>` : "";
+     var insertImage = (message.image)? '<img class="lower-message__image" src="${message.image}">' : "";
+     var html =
+      `<div class = "flex-message" "message_id = ${message.id}">
+             <div class="upper-message">
+               <div class="upper-message__user-name">${message.user_name}</div>
+               <div class="upper-message__date">${message.date}</div>
+             </div>
+             <div class="lower-message">
+               <div class="lower-message__content">${message.content}</div>
+               ${insertImage}
+             </div>
+           </div>`;
+     return html;
+   }
+    //   if ( message.image ) {
+    //     var html =
+    //      `<div class="message" data-message-id=${message.id}>
+    //         <div class="upper-message">
+    //           <div class="upper-message__user-name">
+    //             ${message.user_name}
+    //           </div>
+    //           <div class="upper-message__date">
+    //             ${message.date}
+    //           </div>
+    //         </div>
+    //         <div class="lower-message">
+    //           <p class="lower-message__content">
+    //             ${message.content}
+    //           </p>
+    //         </div>
+    //         <asset_path src=${message.image} >
+    //       </div>`
+    //     return html;
+    //   } else {
+    //     var html =
+    //      `<div class="message" data-message-id=${message.id}>
+    //         <div class="upper-message">
+    //           <div class="upper-message__user-name">
+    //             ${message.user_name}
+    //           </div>
+    //           <div class="upper-message__date">
+    //             ${message.date}
+    //           </div>
+    //         </div>
+    //         <div class="lower-message">
+    //           <p class="lower-message__content">
+    //             ${message.content}
+    //           </p>
+    //         </div>
+    //       </div>`
+    //     return html;
+    //   };
+    // }
 $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -56,11 +73,14 @@ $('#new_message').on('submit', function(e){
        $('.message').append(html);
        $('.message').animate({scrollTop: $('.message')[0].scrollHeight}, 'fast');
        $('form')[0].reset();
+       $('.form__submit').prop('disabled', false);
      })
      .fail(function(){
         alert('error');
       });
-      return false;
-    });
+     .always(function(){
+        $('.submit').prop("disabeled",false);
+      })
+  });
 });
 
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -25,11 +25,13 @@ def update
 
  private
  def group_params
-    params.require(:group).permit(:name, { :user_ids => [] })if @group.update(group_params)
-      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
-    else
-      render :edit
-    end
+    params.require(:group).permit(:name, { :user_ids => [] })
+    # if @group.update(group_params)
+    #   redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+    # else
+    #   render :edit
+    # end
+ end
 end
 
 
