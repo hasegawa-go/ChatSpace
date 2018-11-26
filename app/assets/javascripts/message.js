874e9ebced84c0b427d196aba73be4f2da1984a2
hasegawa-go <gou1982@gmail.com>
2018-11-26 21:04:49 +0900

    指摘いただいた箇所の修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,18 +2,18 @@ $(document).on('turbolinks:load', function(){
    function buildHTML(message){
      var insertImage = (message.image)? '<img class="lower-message__image" src="${message.image}">' : "";
      var html =
-      `<div class = "flex-message" "message_id = ${message.id}">
-             <div class="upper-message">
-               <div class="upper-message__user-name">${message.user_name}</div>
-               <div class="upper-message__date">${message.date}</div>
-             </div>
-             <div class="lower-message">
-               <div class="lower-message__content">${message.content}</div>
-               ${insertImage}
-             </div>
-           </div>`;
+      `<div class = "message" "message_id = ${message.id}">
+          <div class="upper-message">
+            <div class="upper-message__user-name">${message.user_name}</div>
+            <div class="upper-message__date">${message.date}</div>
+          </div>
+          <div class="lower-message">
+            <div class="lower-message__content">${message.content}</div>
+            ${insertImage}
+          </div>
+        </div>`
      return html;
-   }
+  }
 
 $('#new_message').on('submit', function(e){
     e.preventDefault();
@@ -27,18 +27,18 @@ $('#new_message').on('submit', function(e){
       processData: false,
       contentType: false
     })
-     .done(function(data){
-       var html = buildHTML(data);
-       $('.message').append(html);
-       $('.message').animate({scrollTop: $('.message')[0].scrollHeight}, 'fast');
-       $('form')[0].reset();
-     })
-     .fail(function(){
-        alert('error');
-      });
-     .always(function(){
-        $('.submit').prop("disabled",false);
-      })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.message').append(html);
+      $('.message').animate({scrollTop: $('.message')[0].scrollHeight}, 'fast');
+      $('form')[0].reset();
+    })
+    .fail(function(){
+      alert('error');
+    })
+    .always(function(){
+      $('.form__submit').prop('disabled',false);
+    });
   });
 });
 
