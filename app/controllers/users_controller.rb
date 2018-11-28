904e5577047366b84f5b6d186410526d2cebb90b
hasegawa-go <gou1982@gmail.com>
2018-11-28 17:42:16 +0900

    ご指摘箇所の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
 def index
-  @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")where.not("%#{params[:id]current_user}%")
+  @users = User.where('name LIKE(?)', "%#{params[:keyword]}%","%#{params[:id]}%")
 end
 
   def creare
