da090a94bd9f000f0f5c859d02ae63a5cc885804
hasegawa-go <gou1982@gmail.com>
2018-12-02 20:23:46 +0900

    Edit

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,5 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
-set :default_env, {
-  rbenv_root: "/usr/local/rbenv",
-  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
-  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
-  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
-}
-set :linked_files, %w{ config/secrets.yml }
 
 set :application, "ChatSpace"
 set :repo_url, 'git@github.com:hasegawa-go/ChatSpace.git'
@@ -21,6 +14,14 @@ set :ssh_options, auth_methods: ['publickey'],
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
+
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+}
+set :linked_files, %w{ config/secrets.yml }
 
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
